8658baad1c48cfb1d07dd38758ac15e079fb4eb8
chellahsri <48759795+chellahsri@users.noreply.github.com>
2019-03-27 13:57:43 +0530

    Selenium-Framework-config-File

---
+++ Selenium-config1/Selenium-config1/selenium-config-cls.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_config1
+{
+    class selenium_config_cls
+    {
+        public void TestMethod1()
+        {
+            Console.WriteLine(System.Configuration.ConfigurationManager.AppSettings.Get("Browser"));
+            Console.WriteLine(System.Configuration.ConfigurationManager.AppSettings.Get("Username"));
+            Console.WriteLine(System.Configuration.ConfigurationManager.AppSettings.Get("Password"));
+
+        }
+    }
+}
